98b4019b720cb3ae52c53f0e5a96ce6ccebc5854
IsaacLolade <isaaclolade.s337482@cesurformacion.com>
2023-01-27 09:27:05 +0100

    Septimo modificacion ejericio

--- ejercicio.py
+++ ejercicio.py
@@ -9,13 +9,13 @@ while True:
         if cname < 6:
             print("¡ERROR! Su nombre de usuario debe contener al menos 6 carácteres.")
         elif cpswd < 8:
-            print("¡ERROR! La contraseña tiene que contenter al menos 8 carácteres.")
+            print("¡ER ROR! La contraseña tiene que contenter al menos 8 carácteres.")
         else:
-            print("¡ERROR! Tanto el nombre de usuario como la contraseña tienen menos carácteres de los requeridos")
+            print("¡ERRO ! Tanto el nombre de usuario como la contraseña tienen menos carácteres de los requeridos")
     elif cname > 12:
-        print("¡ERROR! Su nombre de usuario no puede contener más de 12 carácteres.")
+        print("¡ERROR! Su n mbre de usuario no puede contener más de 12 carácteres.")
     elif name.isalnum()== False:
-        print("¡ERROR! Su nombre de usuario sólo puede ser alfanumérico.")
+        print("¡ERROR! Su nombre de usuario  sólo puede  ser alfanumérico.")
     while True:
         s = 0
         my = 0
@@ -34,17 +34,17 @@ while True:
             print("¡ERROR! La contraseña no puede contener espacios.")
             break
         elif my < 1:
-            print("¡ERROR! La contraseña debe contener mayusculas.")
+            print("¡ERROR! La contraseña debe contene r mayusculas.")
             break
         elif mn < 1:
-            print("¡ERROR! La contraseña debe contener minusculas.")
+            print("¡ERROR! La contraseña debe con tener minusculas.")
             break
         elif d < 1:
-            print("¡ERROR! La contraseña debe contener números.")
+            print("¡ERROR! La contra seña debe contener números.")
             break
         else:
-            print("Bienvenido ",name)
-            print("Su contraseña es",pswd)
+            print("Bienv enido ",name)
+            print("Su c ontraseña es",pswd)
             break
     else:
         break
